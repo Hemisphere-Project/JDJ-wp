8454b3c95b1970a1d07abe60e0b943adecdd6dd3
Jeremie <jeremie@hemisphere-project.com>
2016-01-12 18:54:50 +0100

    header2

--- header.php
+++ header.php
@@ -30,6 +30,17 @@
 
 			<!-- header -->
 			<header class="header clear" role="banner">
+					<div id='timeheader'>
+						<a href="<?php echo home_url(); ?>">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/time.png">
+						</a>
+					</div>
+					<div id='placeheader'>
+						<a href="<?php echo home_url(); ?>">
+							<img src="<?php echo get_template_directory_uri(); ?>/img/lieu.png">
+						</a>
+					</div>
+
 					<!-- logo -->
 					<div class="logo">
 						<a href="<?php echo home_url(); ?>">
