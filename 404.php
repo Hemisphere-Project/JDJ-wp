870375e1242c1ce33f9044e515101da3c14adfcf
Jeremie <jeremie@hemisphere-project.com>
2016-02-11 15:11:25 +0100

    404

--- 404.php
+++ 404.php
@@ -7,10 +7,10 @@
 			<!-- article -->
 			<article id="post-404">
 				404
-				<h1><?php _e( 'Page not found', 'html5blank' ); ?></h1>
 				<h2>
 					<a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'html5blank' ); ?></a>
 				</h2>
+
 
 			</article>
 			<!-- /article -->
@@ -19,6 +19,5 @@
 		<!-- /section -->
 	</main>
 
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
