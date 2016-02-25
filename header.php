a1628eb13fc6cc59e4a40c25715191fbfa8b03ef
Jeremie <jeremie@hemisphere-project.com>
2016-02-25 12:00:45 +0100

    header SEO

--- header.php
+++ header.php
@@ -12,7 +12,7 @@
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width, initial-scale=1.0">
-		<meta name="description" content="<?php bloginfo('description'); ?>">
+		<meta name="google-site-verification" content="eF5TfaFUMt70ChgiVKLAfDpiCCXaXuBwpR8GV3M1_lg" />
 
 		<?php wp_head(); ?>
 		<script>
